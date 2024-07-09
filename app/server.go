c7e9ba9a56892bffb55fe1299ee77c82b70a9d81
shaik-zeeshan <shaikzeeshan999@gmail.com>
2024-07-09 15:08:40 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"strings"
+
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
@@ -29,22 +31,67 @@ func main() {
 
 		defer conn.Close()
 
-		buf := make([]byte, 1024)
-		_, err = conn.Read(buf)
+		scanner := bufio.NewScanner(conn)
+
+		req, err := ParseRequest(scanner)
+
 		if err != nil {
-			conn.Write([]byte("Could Resolve Your Request"))
-		}
-		reqarr := strings.Split(string(buf), " ")
-
-		url := reqarr[1]
-		if strings.HasPrefix("/echo", url) {
-			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-		} else if strings.HasPrefix(url, "/echo") {
-			suffix := strings.TrimPrefix(url, "/echo/")
-			conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len([]byte(suffix)), suffix)))
-		} else {
-			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+			fmt.Fprintln(conn, "reading input", err)
 		}
 
+		var response string
+
+		switch path := req.Path; {
+		case strings.HasPrefix(path, "/echo"):
+			suffix := strings.TrimPrefix(path, "/echo/")
+			response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len([]byte(suffix)), suffix)
+		case strings.HasPrefix(path, "/user-agent"):
+			response = fmt.Sprintf("%s\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", generateResponse(200, "OK"), len([]byte(req.UserAgent)), req.UserAgent)
+		case path == "/":
+			response = generateResponse(200, "OK") + "\r\n\r\n"
+		default:
+			response = generateResponse(404, "Not Found") + "\r\n\r\n"
+		}
+
+		conn.Write([]byte(response))
 	}
+}
+func generateResponse(statusCode int, statusText string) string {
+	return fmt.Sprintf("HTTP/1.1 %d %s", statusCode, statusText)
+}
+
+type HttpRequest struct {
+	Path      string
+	Method    string
+	Headers   map[string]string
+	Body      string
+	UserAgent string
+}
+
+func ParseRequest(scanner *bufio.Scanner) (*HttpRequest, error) {
+	var options HttpRequest = HttpRequest{}
+	options.Headers = make(map[string]string)
+
+	for i := 0; scanner.Scan(); i++ {
+		if i == 0 {
+			parts := strings.Split(scanner.Text(), " ")
+			options.Method = parts[0]
+			options.Path = parts[1]
+			continue
+		}
+		headers := strings.Split(scanner.Text(), ": ")
+
+		if len(headers) < 2 {
+			options.Body = headers[0]
+			break
+		}
+
+		if headers[0] == "User-Agent" {
+			options.UserAgent = headers[1]
+		}
+
+		options.Headers[headers[0]] = headers[1]
+	}
+
+	return &options, nil
 }
